8da7cd109abee2501885bffc37905c903560f1d1
Adam Silver <adambsilver@gmail.com>
2018-07-05 14:34:10 +0100

    Updated

---
+++ bin/pre-release.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+LATEST_PUBLISHED_TAG=$(git describe --abbrev=0 --tags)
+
+npm run build:package
+
+ALL_PACKAGE_VERSION=$(node -p "require('./package/package.json').version")
+TAG="v$ALL_PACKAGE_VERSION"
+CURRENT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$LATEST_PUBLISHED_TAG" == "$TAG" ]; then
+    echo "⚠️ Git tag already exists."
+    exit 1;
+else
+    git add .
+    git commit -m "Release $TAG"
+    #set upstream so that we can push the branch up
+    git push --set-upstream origin $CURRENT_BRANCH_NAME
+    git push
+    echo "🗒 All done. Ready to create a pull request. Once approved, run npm run release"
+fi
