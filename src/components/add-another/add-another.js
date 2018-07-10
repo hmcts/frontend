fe17195f740081f43d660cd132bc36c98947335c
Adam Silver <adambsilver@gmail.com>
2018-07-10 18:54:05 +0100

    Add another functionality working wip

--- src/components/add-another/add-another.js
+++ src/components/add-another/add-another.js
@@ -1,72 +1,72 @@
 function AddAnother(container) {
-  this.container = $(container);
-  this.container.on('click', '.addAnother-removeButton', $.proxy(this, 'onRemoveButtonClick'));
-  this.container.on('click', '.addAnother-addButton', $.proxy(this, 'onAddButtonClick'));
-  this.container.find('.addAnother-addButton, .addAnother-removeButton').prop('type', 'button');
+	this.container = $(container);
+	this.container.on('click', '.hmcts-add-another__remove-button', $.proxy(this, 'onRemoveButtonClick'));
+	this.container.on('click', '.hmcts-add-another__add-button', $.proxy(this, 'onAddButtonClick'));
+	this.container.find('.hmcts-add-another__add-button, hmcts-add-another__remove-button').prop('type', 'button');
 }
 
 AddAnother.prototype.onAddButtonClick = function(e) {
-  var item = this.getNewItem();
-  this.updateAttributes(this.getItems().length, item);
-  this.resetItem(item);
-  var firstItem = this.getItems().first();
-  if(!this.hasRemoveButton(firstItem)) {
-    this.createRemoveButton(firstItem);
-  }
-  this.getItems().last().after(item);
-  item.find('input, textarea, select').first().focus();
+	var item = this.getNewItem();
+	this.updateAttributes(this.getItems().length, item);
+	this.resetItem(item);
+	var firstItem = this.getItems().first();
+	if(!this.hasRemoveButton(firstItem)) {
+		this.createRemoveButton(firstItem);
+	}
+	this.getItems().last().after(item);
+	item.find('input, textarea, select').first().focus();
 };
 
 AddAnother.prototype.hasRemoveButton = function(item) {
-  return item.find('.addAnother-removeButton').length;
+	return item.find('.hmcts-add-another__remove-button').length;
 };
 
 AddAnother.prototype.getItems = function() {
-  return this.container.find('.addAnother-item');
+	return this.container.find('.hmcts-add-another__item');
 };
 
 AddAnother.prototype.getNewItem = function() {
-  var item = this.getItems().first().clone();
-  if(!this.hasRemoveButton(item)) {
-    this.createRemoveButton(item);
-  }
-  return item;
+	var item = this.getItems().first().clone();
+	if(!this.hasRemoveButton(item)) {
+		this.createRemoveButton(item);
+	}
+	return item;
 };
 
 AddAnother.prototype.updateAttributes = function(index, item) {
-  item.find('[data-name]').each(function(i, el) {
-    el.name = $(el).attr('data-name').replace(/%index%/, index);
-    el.id = $(el).attr('data-id').replace(/%index%/, index);
-    ($(el).prev('label')[0] || $(el).parents('label')[0]).htmlFor = el.id;
-  });
+	item.find('[data-name]').each(function(i, el) {
+		el.name = $(el).attr('data-name').replace(/%index%/, index);
+		el.id = $(el).attr('data-id').replace(/%index%/, index);
+		($(el).prev('label')[0] || $(el).parents('label')[0]).htmlFor = el.id;
+	});
 };
 
 AddAnother.prototype.createRemoveButton = function(item) {
-  item.append('<button type="button" class="secondaryButton addAnother-removeButton">Remove</button>');
+	item.append('<button type="button" class="govuk-button hmcts-button--secondary hmcts-add-another__remove-button">Remove</button>');
 };
 
 AddAnother.prototype.resetItem = function(item) {
-  item.find('[data-name], [data-id]').each(function(index, el) {
-    if(el.type == 'checkbox' || el.type == 'radio') {
-      el.checked = false;
-    } else {
-      el.value = '';
-    }
-  });
+	item.find('[data-name], [data-id]').each(function(index, el) {
+		if(el.type == 'checkbox' || el.type == 'radio') {
+			el.checked = false;
+		} else {
+			el.value = '';
+		}
+	});
 };
 
 AddAnother.prototype.onRemoveButtonClick = function(e) {
-  $(e.currentTarget).parents('.addAnother-item').remove();
-  var items = this.getItems();
-  if(items.length === 1) {
-    items.find('.addAnother-removeButton').remove();
-  }
-  items.each($.proxy(function(index, el) {
-    this.updateAttributes(index, $(el));
-  }, this));
-  this.focusHeading();
+	$(e.currentTarget).parents('.hmcts-add-another__item').remove();
+	var items = this.getItems();
+	if(items.length === 1) {
+		items.find('.hmcts-add-another__remove-button').remove();
+	}
+	items.each($.proxy(function(index, el) {
+		this.updateAttributes(index, $(el));
+	}, this));
+	this.focusHeading();
 };
 
 AddAnother.prototype.focusHeading = function() {
-  this.container.find('.addAnother-heading').focus();
+	this.container.find('.hmcts-add-another__heading').focus();
 };
