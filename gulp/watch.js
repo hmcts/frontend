f4427771795f12ba5a6b52f99b2e0b302bdf17e0
Adam Silver <adambsilver@gmail.com>
2018-11-09 09:48:25 +0000

    Added filter playground, updated sass watch and added scrollable pane

--- gulp/watch.js
+++ gulp/watch.js
@@ -4,6 +4,9 @@ gulp.task('watch-sass', () => {
   return gulp.watch([
     'src/assets/images/**',
     'src/settings/**/*.scss',
+    'src/objects/**/*.scss',
+    'src/helpers/**/*.scss',
+    'src/utilities/**/*.scss',
     'src/components/**/*.scss',
     'src/components/**/*.js',
     'src/namespace.js',
