42f4b9e7b1b193966ae0f72ac5f4edb6c67a0647
Adam Silver <adambsilver@gmail.com>
2018-11-12 09:27:36 +0000

    hmcts width exposed, fleshed out some search styling

--- src/components/search-toggle-button/search-toggle-button.js
+++ src/components/search-toggle-button/search-toggle-button.js
@@ -3,16 +3,15 @@ HMCTSFrontend.SearchToggleButton = function(options) {
   this.toggleButton = $('<button class="hmcts-search-toggle-button" type="button" aria-haspopup="true" aria-expanded="false">'+this.options.toggleButton.text+'</button>');
 	this.toggleButton.on('click', $.proxy(this, 'onToggleButtonClick'));
   this.options.toggleButton.container.append(this.toggleButton);
-  this.options.search.container.addClass('hmcts-hidden');
 };
 
 HMCTSFrontend.SearchToggleButton.prototype.onToggleButtonClick = function() {
   if(this.toggleButton.attr('aria-expanded') == 'false') {
     this.toggleButton.attr('aria-expanded', 'true');
-    this.options.search.container.addClass('hmcts-search--showing');
+    this.options.search.container.removeClass('hmcts-hidden');
     this.options.search.container.find('input').first().focus();
 	} else {
-		this.options.search.container.removeClass('hmcts-search--showing');
+		this.options.search.container.addClass('hmcts-hidden');
 		this.toggleButton.attr('aria-expanded', 'false');
 	}
 };
