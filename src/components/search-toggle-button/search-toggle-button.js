f86524cb79957ed2864deca8ff4284180ac40a2e
Trevor Saint <hello@trevorsaint.com>
2018-11-12 15:23:48 +0000

    Primary navigation and search work

--- src/components/search-toggle-button/search-toggle-button.js
+++ src/components/search-toggle-button/search-toggle-button.js
@@ -1,6 +1,6 @@
 HMCTSFrontend.SearchToggleButton = function(options) {
   this.options = options;
-  this.toggleButton = $('<button class="hmcts-search-toggle-button" type="button" aria-haspopup="true" aria-expanded="false">'+this.options.toggleButton.text+'</button>');
+  this.toggleButton = $('<button class="hmcts-search-toggle__button" type="button" aria-haspopup="true" aria-expanded="false">'+this.options.toggleButton.text+'</button>');
 	this.toggleButton.on('click', $.proxy(this, 'onToggleButtonClick'));
   this.options.toggleButton.container.append(this.toggleButton);
 };
